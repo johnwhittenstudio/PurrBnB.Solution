c831b702defc2414dd5cd692211f413c54594d56
Phil Curran <pecurran@hotmail.com>
2022-04-27 18:03:29 -0700

    bunch of styling updates.

--- PurrBnB/Controllers/HomeController.cs
+++ PurrBnB/Controllers/HomeController.cs
@@ -22,12 +22,7 @@ namespace PurrBnB.Solution.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
