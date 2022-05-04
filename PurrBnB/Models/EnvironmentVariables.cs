696a7cdad5c0d8f455c4a2da73a694a1287a9bfa
Christen Weston <paleeserecycle@gmail.com>
2022-05-03 18:08:31 -0700

    Added environment variables

--- PurrBnB/Models/EnvironmentVariables.cs
+++ PurrBnB/Models/EnvironmentVariables.cs
@@ -3,5 +3,7 @@ namespace PurrBnB.Models
     public static class EnvironmentVariables
     {
         public static string ApiKeyGeo = "ed4defa185064c72860cbff688f2edd2";
+        public static string ApiKeyGoogle = "AIzaSyDsClsSHwNMxgrsZ9uok8PihFe2ugXUI9s";
+
     }
 }
